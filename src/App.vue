c904d6ef12ed660d5baec435ed20325483562b96
quangnx <nguyenxuanquang.bg@gmail.com>
2022-12-07 16:49:44 +0700

    router links

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about"><button>edit</button></router-link>
   </nav>
   <router-view/>
 </template>
