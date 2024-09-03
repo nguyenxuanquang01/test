a10841e606321b0cdfb6da11ecc0e7fec31bef4b
Quang Nguyen <nguyenxuanquang.bg@gmail.com>
2024-09-03 10:05:16 +0700

    v2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ServiceService } from './mat-table-custom/service.service';
 import { MatTableDataSource } from '@angular/material/table';
+import { delay } from 'rxjs';
 
 export interface PeriodicElement {
 
@@ -12,6 +13,8 @@ export interface PeriodicElement {
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  isprogress: boolean = false;
+
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
   private dataBuffer: any[] = [];
   private currentOffset = 0;
@@ -26,12 +29,15 @@ export class AppComponent {
   }
 
   fetchData() {
+    this.isprogress = true;
     this.dataService.getData()
       .subscribe(data => {
+        console.log("Here");
         this.dataBuffer = this.dataBuffer.concat(data);
+        this.totalLength = this.dataBuffer.length;
         this.dataSource.data = this.dataBuffer;
         this.currentOffset += this.limit;
+        this.isprogress = false;
       });
   }
-
 }
--- src/app/mat-table-custom/mat-table-custom.component.ts
+++ src/app/mat-table-custom/mat-table-custom.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { AfterViewInit, ViewChild } from '@angular/core';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
@@ -12,7 +12,7 @@ export interface PeriodicElement {
   templateUrl: './mat-table-custom.component.html',
   styleUrls: ['./mat-table-custom.component.css']
 })
-export class MatTableCustomComponent implements OnInit {
+export class MatTableCustomComponent implements OnChanges {
   @Input() displayedColumns: string[] = [];
   @Input() totalLength: number | undefined;
   @Input() dataSource = new MatTableDataSource<PeriodicElement>();
@@ -30,19 +30,35 @@ export class MatTableCustomComponent implements OnInit {
   constructor() { }
 
 
-  ngOnInit() {
-    this.dataBuffer = this.dataSource.data;
-    const startIndex = 0;
-    const endIndex = 5;
-    const paginatedData = this.dataBuffer.slice(startIndex, endIndex);
-    this.dataSource.data = paginatedData;
-  }
+  // ngOnInit() {
+  //   this.dataBuffer = this.dataSource.data;
+  //   const startIndex = 0;
+  //   const endIndex = 5;
+  //   console.log(5);
+  //   const paginatedData = this.dataBuffer.slice(startIndex, endIndex);
+  //   this.dataSource.data = paginatedData;
+  // }
 
 
   ngOnChanges(changes: SimpleChanges) {
-    if (changes['dataSource'] && this.paginator) {
+    if (this.paginator) {
       this.paginator.length = this.totalLength; // Update paginator length
+      console.log('paginator');
+
     }
+
+    if (changes['totalLength']) {
+
+      this.doSomethingAfterDataSourceUpdate();
+    }
+  }
+
+  doSomethingAfterDataSourceUpdate() {
+    console.log('Number of items in dataSource:', this.dataSource.data.length);
+    const startIndex = 0;
+    const endIndex = 5;
+    //   this.dataSource.data = paginatedData;
+    this.dataBuffer = this.dataSource.data.slice(startIndex, endIndex);;
   }
 
   onPageChange(event: PageEvent) {
@@ -50,9 +66,9 @@ export class MatTableCustomComponent implements OnInit {
     this.page_Index = event.pageIndex;
     const startIndex = event.pageIndex * event.pageSize;
     const endIndex = startIndex + event.pageSize;
-    const paginatedData = this.dataBuffer.slice(startIndex, endIndex);
-    this.dataSource.data = paginatedData;
-    // if (endIndex > 10) {
+    this.dataBuffer = this.dataSource.data.slice(startIndex, endIndex);
+
+    // if (endIndex > this.dataBuffer.length) {
     //   this.paginator.length = 100;
     //   // Fetch more data
     // }
